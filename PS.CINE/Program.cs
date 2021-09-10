1acd74e67955fded79c2aac51a4d9c66d887c7b2
EmmanuelJulio <emajulio.ej@gmail.com>
2021-09-10 14:56:31 -0300

    Arquitectura de capas echa

---
+++ PS.CINE/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PS.CINE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ PS.DATE/Command/GenericRepository.cs
@@ -0,0 +1,32 @@
+﻿using PS.DOMAIN.Comands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PS.DATE.Command
+{
+    public class GenericRepository : IGenericsRepository
+    {
+        public void Add<T>(T entity) where T : class
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete<T>(T entity) where T : class
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> GetALL<T>() where T : class
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update<T>(T entity) where T : class
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
